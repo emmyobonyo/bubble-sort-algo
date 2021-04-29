f70840d8cd6a59b9feac5f6cc5b34158f38f8770
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-04-29 17:26:12 +0300

    Create method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,13 @@
-def name
-  'name'
+def bubble_sort(array)
+  i = 0
+  while (i < array.length-1) do
+    if array[i]>array[i+1]
+      array[i+1] = array.push([i])
+    end
+    i = i+1
+  end
+  p array
 end
+
+a = [2, 5, 3, 6, 7, 5, 4, 8, 9]
+bubble_sort(a)
