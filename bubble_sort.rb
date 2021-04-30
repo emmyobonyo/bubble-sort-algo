59b327295a9b253cfd8bbefdcea1b41aa5754b33
tmampa <tmampa94@gmail.com>
2021-04-30 11:09:21 +0200

    Add yield to sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,17 +2,33 @@ def bubble_sort(array)
   k = array.length
 
   loop do
-    sorted = false
+    unsorted = false
     (k - 1).times do |i|
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         sorted = true
       end
     end
-    break unless sorted
+    break unless unsorted
+  end
+  array
+end
+
+def bubble_sort_by(array)
+  k = array.length
+
+  loop do
+    unsorted = false
+    (k - 1).times do |i|
+      if yield (array[i] > array[i + 1]) >= 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = true
+      end
+    end
+    break unless unsorted
   end
   array
 end
 
 a = [5, 8, 4, 3, 8, 4, 7, 7, 5]
-p bubble_sort(a)
+p bubble_sort_by(a)
